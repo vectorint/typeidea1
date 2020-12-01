c55a10cf1c912c946ea46ed73644824e436f9aa2
jackliang <493259041@qq.com>
2020-12-01 20:57:46 +0800

    on_delete

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 	name = models.CharField(max_length=50, verbose_name='名称')
 	status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
 	is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
-	owner = models.ForeignKey(User, verbose_name='作者')
+	owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
 	created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
 	class Meta:
@@ -32,7 +32,7 @@ class Tag(models.Model):
 
 	name = models.CharField(max_length=10, verbose_name='名称')
 	status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
-	owner = models.ForeignKey(User, verbose_name='作者')
+	owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
 	created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
 	class Meta:
@@ -55,7 +55,7 @@ class Post(models.Model):
 	status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
 	category = models.ForeignKey(Category, verbose_name='分类')
 	tag = models.ManyToManyField(Tag, verbose_name='标签')
-	owner = models.ForeignKey(User, verbose_name='作者')
+	owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
 	created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
 	class Meta:
