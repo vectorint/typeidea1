16cf4cbfe1629f2af722065708abe370c213203d
jackliang <493259041@qq.com>
2020-12-16 20:05:08 +0800

    add html

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tag, Post, Category
 
 
 # Create your views here.
 
 
 def post_list(request, category_id=None, tag_id=None):
-	return render(request, 'blog/list.html', context={'name': 'post_list'})
+	tag = None
+	category = None
+	if tag_id:
+		try:
+			tag = Tag.objects.get(id=tag_id)
+		except Tag.DoesNotExist:
+			post_list = []
+		else:
+			post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+	else:
+		post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+		if category_id:
+			try:
+				category = Category.objects.get(id=category_id)
+			except Category.DoesNotExist:
+				category = None
+			else:
+				post_list = Post.objects.filter(category_id=category_id)
+	return render(request, 'blog/list.html', context={'post_list': post_list, 'tag': tag, 'category': category})
 
 
-def post_detail(request, post_id):
-	return HttpResponse('detail')
+def post_detail(request, post_id=None):
+	try:
+		post = Post.objects.get(id=post_id)
+	except Post.DoesNotExist:
+		post = None
+	return render(request, 'blog/detail.html', context={'post': post})
